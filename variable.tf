8934e05796174d79a978e98949bb7a225482c77c
mniranjanr7 <mniranjanr7@gmail.com>
2025-09-02 02:27:48 +0530

    Add initial Terraform configuration for Azure
    Introduces Terraform files to manage Azure resources, including provider setup, backend configuration for remote state, resource group creation, and variable definitions. Also adds .gitignore to exclude Terraform state files.

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "rg-name" {
+    description = "This is the rg name"
+    default = "RG-CDO-PIQT"
+    type = string
+}
+
+variable "location" {
+    description = "This is the location"
+    default = "West Europe"
+    type = string
+}
