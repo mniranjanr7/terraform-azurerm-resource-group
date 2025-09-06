5d08e29049e09b9300f7f4a37ab96fe4fa58a970
Terraform-Repo <mniranjanr7@gmail.com>
2025-09-06 16:50:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 resource "azurerm_resource_group" "rg-create" {
   name = var.rg-name
   location = var.location
+  tags = {
+    project = var.project
+    location = var.location
+    snow-queue = var.snowqueue
+}
 }
