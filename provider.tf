a08c3e63d057829f843d14884babaa9e912088a2
mniranjanr7 <mniranjanr7@gmail.com>
2025-09-09 11:56:17 +0530

    Comment out provider configuration in provider.tf
    All provider and Terraform block configurations have been commented out in provider.tf, possibly to disable them temporarily or for refactoring purposes.

--- provider.tf
+++ provider.tf
@@ -1,23 +1,23 @@
-terraform {
-  required_version = "~>1.13.0"
-    required_providers {
-        azurerm = {
-        source  = "hashicorp/azurerm"
-        version = ">= 4.0.0"
-        }
+# terraform {
+#   required_version = "~>1.13.0"
+#     required_providers {
+#         azurerm = {
+#         source  = "hashicorp/azurerm"
+#         # version = ">= 4.0.0"
+#         }
 
-        hcp = {
-          source  = "hashicorp/hcp"
-          version = ">= 0.13.0"
-        }
-    }
-}
+#         hcp = {
+#           source  = "hashicorp/hcp"
+#           # version = ">= 0.13.0"
+#         }
+#     }
+# }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "eed6d1d9-7608-45be-998a-5c067ccfac94"
-}
-provider "hcp" {
-  # No additional attributes are required for the hcp provider here.
+# provider "azurerm" {
+#   features {}
+#   subscription_id = "eed6d1d9-7608-45be-998a-5c067ccfac94"
+# }
+# provider "hcp" {
+#   # No additional attributes are required for the hcp provider here.
   
-}
+# }
