f3bcc18f3380324a58947338e4129b385751d3b3
mniranjanr7 <mniranjanr7@gmail.com>
2025-09-09 12:37:44 +0530

    Refactor resource group to support multiple deployments
    Replaces individual resource group variables with a map variable 'rg-deploy' to enable deploying multiple resource groups. Updates resource definition to use for_each and per-resource attributes, improving scalability and flexibility.

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+# resource "azurerm_resource_group" "resource_group" {
+#   name = var.rg-name
+#   location = var.location 
+#   tags = local.tags
+# }
+
 resource "azurerm_resource_group" "resource_group" {
-  name = var.rg-name
-  location = var.location 
-  tags = local.tags
+  for_each = var.rg-deploy
+  name     = each.value.rg-name
+  location = each.value.location
+  tags     = {
+    Project        = each.value.project
+    Snow-Queue-Name = each.value.Snow-Queue-Name
+  }
+  
 }
--- variable.tf
+++ variable.tf
@@ -1,23 +1,34 @@
-variable "rg-name" {
-    description = "This is the rg name"
-    default = "RG-CDO-PIQT"
-    type = string
-}
+# variable "rg-name" {
+#     description = "This is the rg name"
+#     default = "RG-CDO-PIQT"
+#     type = string
+# }
 
-variable "location" {
-    description = "This is the location"
-    default = "West Europe"
-    type = string
-}
+# variable "location" {
+#     description = "This is the location"
+#     default = "West Europe"
+#     type = string
+# }
 
-variable "project" {
-    description = "This is the project"
-    default = "CDO"
-    type = string
-}
+# variable "project" {
+#     description = "This is the project"
+#     default = "CDO"
+#     type = string
+# }
 
-variable "snowqueue" {
-    description = "This is the snowqueue"
-    default = "Data-Platform"
-    type = string
+# variable "snowqueue" {
+#     description = "This is the snowqueue"
+#     default = "Data-Platform"
+#     type = string
+# }
+
+variable "rg-deploy" {
+  description = "Flag to deploy Resource Group"
+  type = map(object({
+    Snow-Queue-Name = string
+    project         = string
+    location        = string
+    rg-name         = string
+
+  }))
 }
